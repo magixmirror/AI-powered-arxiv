7c33d45abc830bf74da727c95dc100c7c908812d
magixmirror <goldmagixmirror@gmail.com>
2024-03-09 18:46:47 +0000

    #8 Update tests for search module

--- tests/test_search.py
+++ tests/test_search.py
@@ -5,8 +5,8 @@ from aixiv.search import search
 # constants
 CATEGORIES = ("astro-ph.GA",)
 KEYWORDS = ("galaxy",)
-START_DATE = "2021-01-01 in UTC"
-END_DATE = "2021-01-02 in UTC"
+START = "2021-01-01 in UTC"
+END = "2021-01-02 in UTC"
 EXPECTED_URLS = [
     "http://arxiv.org/abs/2101.00188v2",
     "http://arxiv.org/abs/2101.00158v1",
@@ -17,11 +17,6 @@ EXPECTED_URLS = [
 
 # test functions
 def test_search() -> None:
-    articles = search(
-        categories=CATEGORIES,
-        keywords=KEYWORDS,
-        start_date=START_DATE,
-        end_date=END_DATE,
-    )
+    articles = search(CATEGORIES, KEYWORDS, START, END)
     urls = [article.url for article in articles]
     assert urls == EXPECTED_URLS
