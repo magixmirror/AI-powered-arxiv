3d24f3d915576ad13d706bd7e450badc8b72572f
magixmirror <goldmagixmirror@gmail.com>
2024-03-17 07:02:52 +0000

    #14 Update Google translator

--- aixiv/translators/google.py
+++ aixiv/translators/google.py
@@ -2,8 +2,9 @@ __all__ = ["Google"]
 
 
 # standard library
-from dataclasses import replace
+from dataclasses import dataclass, replace
 from logging import getLogger
+from time import sleep
 
 
 # dependencies
@@ -19,6 +20,7 @@ PROMPT_TRANSLATE = "Strictly translate the following texts in {language}."
 PROMPT_SUMMARIZE = "Summarize the following texts in {language}."
 
 
+@dataclass
 class Google(Translator):
     """Translator by Google generative models.
 
@@ -26,23 +28,26 @@ class Google(Translator):
         api_key: API key of the translator.
         language: Language code of the translated articles.
         summarize: Whether to summarize the articles.
+        latency: Latency to avoid exceeding the API rate limit.
         model: Name of the generative model.
 
     """
+
+    latency: float = 1.0
+    """Latency to avoid exceeding the API rate limit."""
 
     model: str = "gemini-pro"
     """Name of the generative model."""
 
     async def __call__(self, article: TArticle, /) -> TArticle:
         """Translate (and summarize) an article."""
-        # lazy import
+        # create model
         from google import generativeai as genai
 
         genai.configure(api_key=self.api_key)
         model = genai.GenerativeModel(self.model)
-        run = model.generate_content_async
 
-        # convert language code to language name in English
+        # create prompt w/o texts
         language = Locale.parse(self.language).get_language_name(LANG_EN)
 
         if self.summarize:
@@ -50,14 +55,17 @@ class Google(Translator):
         else:
             prompt = PROMPT_TRANSLATE.format(language=language)
 
-        try:
-            resp_title = await run(f"{prompt}\n{article.title}")
-            resp_summary = await run(f"{prompt}\n{article.summary}")
+        # run translations
+        async def run(prompt: str) -> str:
+            sleep(self.latency)  # sync sleep!
+            response = await model.generate_content_async(prompt)
+            return response.text
 
+        try:
             return replace(
                 article,
-                title=resp_title.text,
-                summary=resp_summary.text,
+                title=await run(f"{prompt}\n{article.title}"),
+                summary=await run(f"{prompt}\n{article.summary}"),
             )
         except Exception as error:
             LOGGER.warning(error)
